be2b59f6d10cc4fee2a1540208ae126bdf1e0359
Coursera Learner <nobody@nowhere.nul>
2020-03-22 01:32:53 +0000

    got canonical working.  now need to do the 0 cases

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,32 +16,47 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-struct rect_tag {
+typedef struct rect_tag {
   int x;
   int y;
   int width;
   int height;
-};
+} rectangle;
 
-typedef struct rect_tag rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
   if (r.width < 0) {
     r.x = r.x + r.width;
-    r.width = r.width * -1;
+    r.width = (r.width * -1);
+
   }
 
   if (r.height < 0) {
     r.y = r.y + r.height;
     r.height = r.height * -1;
   }
+  
   return r;
 }
+
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
+
+  //printf("\n begin intersection function \n");
+  
+  r1 = canonicalize(r1);
+  //printf("testing print canonacalized r1 \n");
+  //printf("%d, %d, %d, %d, \n", r1.x, r1.y, r1.width, r1.height);
+    
+  r2 = canonicalize(r2);
+  //printf("testing print canonacalized r2\n");
+  //printf("%d, %d, %d, %d, \n", r2.x, r2.y, r2.width, r2.height);
+  
   int right = min((r1.x + r1.width),(r2.x + r2.width));
+  //printf("right = %d \n", right);
   int top = min((r1.y + r1.height),(r2.y + r2.height));
+  //printf("top = %d \n", top);
   rectangle i;
   i.x = max(r1.x, r2.x);
   i.y = max(r1.y, r2.y);
