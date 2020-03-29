cebe5c1c18baeed555aec75d05c40dcc3268ef97
Brandon Stranzl <brandonstranzl@Brandons-MacBook-Pro.local>
2020-03-29 12:05:15 -0400

    have both working and retirement working

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -16,8 +16,6 @@ void print_balance(
 		retire_info working,
 		retire_info retirement) {
 
-  printf("*******hello********* \n*******************\n");
-
   for (int i = 0; i < working.months; i++) {
     //double balance = beg_bal; 
     int months = (beg_age + i) % 12; 
@@ -28,8 +26,8 @@ void print_balance(
 
    for (int i = 0; i < retirement.months; i++) {
     //double balance = beg_bal; 
-    int months = (working.months + i) % 12; 
-    int years = ((working.months + i) / 12);
+    int months = (beg_age + working.months + i) % 12; 
+    int years = ((beg_age + working.months + i) / 12);
     printf("Age %3d month %2d you have $%.2lf\n", years, months, beg_bal);
     beg_bal = (beg_bal * (1 + (retirement.rate / 12))) + retirement.contribution;
   }
@@ -39,18 +37,22 @@ void print_balance(
 int main(void) {
 
   retire_info working;
-  working.months = 36;
-  working.contribution = 100;
-  working.rate = .00;
+  working.months = 489;
+  working.contribution = 1000;
+  working.rate = .045;
 
   retire_info retirement;
-  retirement.months = 36;
-  retirement.contribution = -100;
-  retirement.rate = .00;
+  retirement.months = 384;
+  retirement.contribution = -4000;
+  retirement.rate = .01;
     
   int beg_age = 327;
   double beg_bal = 21345;
-
+  
   print_balance(beg_age, beg_bal, working, retirement);
-    
+
+  //  printf("\n ** months working %d ** \n", working.months);
+  //printf("\n ** years working %d ** \n", (working.months / 12));
+  //printf("\n ** retirement age %d  ** \n",(working.months / 12) + (beg_age /12));
+  
 }
