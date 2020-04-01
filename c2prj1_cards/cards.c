1452d910437034d6ee7677f6b8c6021ac0cc45a5
Coursera Learner <nobody@nowhere.nul>
2020-04-01 02:55:48 +0000

    added to value_from_letter but in switch/case

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -25,7 +25,21 @@ const char * ranking_to_string(hand_ranking_t r) {
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  switch (c.value) {
+  case 2: return "2";
+  case 3: return "3";
+  case 4: return "4";
+  case 5: return "5";
+  case 6: return "6";
+  case 7: return "7";
+  case 8: return "8";
+  case 9: return "9";
+  case 10: return "0";
+  case 11: return "J";
+  case 12: return "Q";
+  case 13: return "K";
+  case 14: return "A";
+  default: return 'x';
 }
 
 
