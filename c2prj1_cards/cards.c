9c11c1eb6656e42f84733c0f95c20ccaf47a419a
Coursera Learner <nobody@nowhere.nul>
2020-04-02 01:47:25 +0000

    have it working using https://github.com/mvirgo/c-programming/blob/master/c2prj1_cards/cards.c

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -24,40 +24,74 @@ const char * ranking_to_string(hand_ranking_t r) {
   }
 }
 
+
 char value_letter(card_t c) {
   switch (c.value) {
-  case 2: return "2";
-  case 3: return "3";
-  case 4: return "4";
-  case 5: return "5";
-  case 6: return "6";
-  case 7: return "7";
-  case 8: return "8";
-  case 9: return "9";
-  case 10: return "0";
-  case 11: return "J";
-  case 12: return "Q";
-  case 13: return "K";
-  case 14: return "A";
-  default: return 'x';
+  case 2: return '2';
+  case 3: return '3';
+  case 4: return '4';
+  case 5: return '5';
+  case 6: return '6';
+  case 7: return '7';
+  case 8: return '8';
+  case 9: return '9';
+  case 10: return '0';
+  case VALUE_ACE: return 'A';
+  case VALUE_KING: return 'K';
+  case VALUE_QUEEN: return 'Q';
+  case VALUE_JACK: return 'J';
+  default: return '?';
+  }
 }
-
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit) {
+  case SPADES: return 's';
+  case HEARTS: return 'c';
+  case DIAMONDS: return 'h';
+  case CLUBS: return 'd';
+  default: return '?';  
+  }
 }
-
+  
 void print_card(card_t c) {
-
+  char value = value_letter(c);
+  char suit = suit_letter(c);
+  printf("%c%c", value, suit);
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
-  card_t temp;
-  return temp;
+  card_t c;
+  switch (value_let) {
+  case '2': c.value = 2; break;
+  case '3': c.value = 3; break;
+  case '4': c.value = 4; break;
+  case '5': c.value = 5; break;
+  case '6': c.value = 6; break;  
+  case '7': c.value = 7; break;
+  case '8': c.value = 8; break;
+  case '9': c.value = 9; break;
+  case '0': c.value = 10; break;
+  case 'J': c.value = VALUE_JACK; break;
+  case 'Q': c.value = VALUE_QUEEN; break;
+  case 'K': c.value = VALUE_KING; break;
+  case 'A': c.value = VALUE_ACE; break;
+  }
+  switch (suit_let) {
+  case 's': c.suit = SPADES; break;
+  case 'h': c.suit = HEARTS; break;
+  case 'd': c.suit = DIAMONDS; break;
+  case 'c': c.suit = CLUBS; break;
+  }
+  assert_card_valid(c);
+  return(c);
 }
+    
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  temp.value = (c / 4) + 2;
+  temp.suit = c%4;
   return temp;
 }
+
