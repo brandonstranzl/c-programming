170226491833739fdf0375a82cd455b669e800ef
Coursera Learner <nobody@nowhere.nul>
2020-04-12 14:36:54 +0000

    commit of pointers

--- 12_read_ptr2/test.c
+++ 12_read_ptr2/test.c
@@ -15,6 +15,15 @@ int f(int ** r, int ** s) {
   return temp + temp2;
 }
 
+/*Note that r is a double pointer. This means that (*r) dereferences the pointer once to match the type of z, int *. Thus z is a pointer to the same value that the second pointer in r** points to. Imagine r consists of three parts - the first pointer, the second pointer and the value. *r retrieves the value of the second pointer. I've drawn some arrows below to help visualize.
+
+int ** r has a type that implies it consists of: ptr1 -> ptr2 -> value
+
+*r dereferences once so we get: ptr2 -> value
+
+int * z = *r thus implies z = ptr2->value*/
+
+
 int main(void) {
   int a = 80;
   int b = 12;
