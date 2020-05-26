526b92536de0fca5d4f56d704c2866c7b767a081
Brandon Stranzl <brandonstranzl@Brandons-MacBook-Pro.local>
2020-05-25 20:43:54 -0400

    updates to c3prj

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -7,23 +7,7 @@
 void assert_card_valid(card_t c) {
   assert(c.value >= 2 && c.value <= VALUE_ACE);
   assert(c.suit >= SPADES && c.suit <= CLUBS);    
-};
-
-const char * ranking_to_string(hand_ranking_t r) {
-  switch (r) {
-  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
-  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
-  case FULL_HOUSE: return "FULL_HOUSE";
-  case FLUSH: return "FLUSH";
-  case STRAIGHT: return "STRAIGHT";
-  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
-  case TWO_PAIR: return "TWO_PAIR";
-  case PAIR: return "PAIR";
-  case NOTHING: return "NOTHING";
-  default: return "???";
-  }
 }
-
 
 char value_letter(card_t c) {
   switch (c.value) {
@@ -87,7 +71,6 @@ card_t card_from_letters(char value_let, char suit_let) {
   return(temp);
 }
     
-
 card_t card_from_num(unsigned c) {
   card_t temp;
   temp.value = (c / 4) + 2;
@@ -95,3 +78,17 @@ card_t card_from_num(unsigned c) {
   return temp;
 }
 
+const char * ranking_to_string(hand_ranking_t r) {
+  switch (r) {
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
+  case FLUSH: return "FLUSH";
+  case STRAIGHT: return "STRAIGHT";
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
+  case PAIR: return "PAIR";
+  case NOTHING: return "NOTHING";
+  default: return "???";
+  }
+}
--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,18 +2,62 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
 
+void print_hand(deck_t * hand){
+  for(size_t i=0; i<hand->n_cards; i++){
+    card_t card = **(hand->cards+i);
+    print_card(card);
+    printf(" ");
+  }
 }
 
 int deck_contains(deck_t * d, card_t c) {
+  for(size_t i=0; i<d->n_cards; i++){
+    card_t card = **((d->cards)+i);
+    if (card.value == c.value && card.suit == c.suit) {
+      return 1;
+    }
+  }
   return 0;
 }
 
 void shuffle(deck_t * d){
-
+  card_t ** cards = d->cards;
+  size_t n = d->n_cards;
+  //printf("\nhere is the n in shuffle %lu\n", n);
+  int r;
+  for(size_t i = n - 1; i > 0; i--){
+    r = rand() % (i + 1);
+    //printf("%d  ", r);
+    card_t temp = **(cards + i);
+    **(cards + i) = **(cards + r);
+    **(cards + r) = temp;
+    // **((d->cards)+r) = **((d->cards)+i);
+    // **((d->cards)+i) = **temp; 
+  }
+  //printf("\n");
 }
 
 void assert_full_deck(deck_t * d) {
-
-}
+  // printf("here is the assert deck:\n");
+  // print_hand(d);
+  // printf("\n");
+  // printf("size of d = %zu\n", d->n_cards);
+  int count = 0;
+  for(size_t i=0; i<d->n_cards; i++){
+    card_t card1 = **(d->cards+i);
+    //print_card(card1);
+    //printf("\n");
+    //printf(" YIKES %u%c\n", card1.value, suit_letter(card1));
+    for(size_t j=0; j<d->n_cards; j++){
+      card_t card2 = **(d->cards+j);
+      //print_card(card2);  
+      if(card2.value==card1.value && card2.suit == card1.suit) { 
+         count = 1;
+      }
+      //printf(" ");
+    }
+    //printf("\ncount = %d\n", count);
+  }
+  assert(count == 1);
+} 
