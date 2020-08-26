8795b2e0bf1b6d38af4dc539938dbea2adb2b0f9
Brandon Stranzl <brandonstranzl@Brandons-MacBook-Pro.local>
2020-08-26 11:21:07 -0400

    final

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -3,75 +3,80 @@
 
 void rotate(char matrix[10][10]) {
   char rotated[10][10];
-  if(matrix != NULL)
-  {
-  for (int i = 0; i < 10; i ++) {
-    for (int j = 0; j < 10; j++) {
-      rotated[i][j] = matrix[10-1-j][i];
+    if(matrix != NULL) {
+      for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 10; j++) {
+        rotated[i][j] = matrix[10-1-j][i];
+      }
     }
-  }
-  for (int i = 0; i < 10; i ++) {
-    for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < 10; i ++) {
+      for (int j = 0; j < 10; j++) {
       matrix[i][j] = rotated[i][j];
+      }
     }
-  }
   }
 }
+
+void readMatrix(FILE * f, char matrix[10][10]) {
+  int c;
+  int numLines = 0;
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      c = fgetc(f);
+      //printf("%c", c);
+      if(c == EOF) {
+        fprintf(stderr,"Empty File\n");
+        exit(EXIT_FAILURE);
+      }
+      if (c == '\n') {
+       fprintf(stderr,"line %d too short, %c = \\n at %d\n",i,c,j);
+       exit(EXIT_FAILURE);
+      }
+      else {
+      //write the first 10 chars of a row to the matrix
+       matrix[i][j] = c;
+      }
+    }
+    //chomp off next char make sure it's a '\n'
+    if ( (c = fgetc(f)) != '\n' ) {
+      fprintf(stderr,"Invalid input: line %d too long\n",i);
+      exit(EXIT_FAILURE);
+    }
+    //else {
+    //  printf("\n");
+    // }
+    numLines = i;
+  }
+  //fail if too few rows read
+  if (numLines != 9) {
+    fprintf(stderr,"Not enough rows\n");
+    exit(EXIT_FAILURE);
+  }
+  //count each row read, fail if too many rows read
+  if ( (c=fgetc(f)) != EOF) {
+    fprintf(stderr,"Input file is too long\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
 
 int main(int argc, char ** argv) {
  
   if (argc != 2) {
     fprintf(stderr,"Invalid number of params in argv!\n");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   FILE * f = fopen(argv[1], "r");
 
   if (f == NULL) {
-    fprintf(stderr,"Invalid input file '%s' - input file == NULL\n",argv[1]);
-    exit(EXIT_FAILURE);
-  }
-
-  /*  if (*f == NULL) {
-    fprintf(stderr,"Invalid input file '%s' - input file == NULL\n",argv[1]);
-    exit(EXIT_FAILURE);
-    }*/
-
-  /* int c = fgetc(f);
-  if(c == EOF) {
-    fprintf(stderr,"Usage: Empty File\n");
+    fprintf(stderr,"Invalid input file\n");
     return EXIT_FAILURE;
-    }*/
-
+  }
+  
   char matrix[10][10];
 
-  for (int i = 0; i < 10; i++) {
-    char temp[12];
-    if ( fgets(temp, 12, f) == NULL ) {
-      exit(EXIT_FAILURE);
-    }
-    if (temp[10] == '\n') {
-      for (int j = 0; j < 10; j++) {
-	if (temp[j] != '\n') {
-	  matrix[i][j] = temp[j];
-	}
-	else {
-	  fprintf(stderr,"Invalid input file '%s' - newline in wrong place\n",argv[1]);
-	  exit(EXIT_FAILURE);
-	}
-      }
-    }
-    else {
-      fprintf(stderr,"Invalid input in %s\n",argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  }
-  int c2=fgetc(f);
-  if (c2 != EOF) {
-    fprintf(stderr,"Input file '%s' is too long\n",argv[1]);
-    exit(EXIT_FAILURE);
-  }
-
+  readMatrix(f, matrix);
   rotate(matrix);
 
   for (int i = 0; i < 10; i++) {
