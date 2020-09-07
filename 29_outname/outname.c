44947ad41a9590d6be368f5cad1a77975df0d3b0
Coursera Learner <nobody@nowhere.nul>
2020-09-07 20:15:10 +0000

    committing outname

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include "outname.h"
 
+//.counts  + /0
 char * computeOutputFileName(const char * inputName) {
+  char * outFileName = malloc((strlen(inputName) + 8)  * sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+  //free(outFileName);
+  return outFileName;
   //WRITE ME
 }
+
