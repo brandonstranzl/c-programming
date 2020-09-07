657c68464b7de0ebf439184c8bf2debd7e411bfc
Brandon Stranzl <brandonstranzl@Brandons-MacBook-Pro.local>
2020-09-07 16:19:17 -0400

    committing

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include "outname.h"
 
+//.counts  + /0
 char * computeOutputFileName(const char * inputName) {
+  char * outFileName = malloc((strlen(inputName) + 8)  * sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+  //free(outFileName);
+  return outFileName;
   //WRITE ME
 }
