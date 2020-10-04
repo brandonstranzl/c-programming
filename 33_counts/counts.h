ae6929fe8ebaf3b05fee22a291d15166d7069a68
Coursera Learner <nobody@nowhere.nul>
2020-10-04 01:31:48 +0000

    committing 33_counts

--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -1,14 +1,20 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
+
 struct _one_count_t {
   //DEFINE ME
-
+  char * string;
+  int count;
 };
+
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** array;
+  size_t size;
 };
+
 typedef struct _counts_t counts_t;
 
 counts_t * createCounts(void);
